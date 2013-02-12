951f582c145d519fcb744fecbbff42ca40978b8a
Attila Szegedi <attila@openjdk.org>
2013-02-12 12:47:51 +0100

    8007900: Function binding is inefficient
    Reviewed-by: jlaskey, lagergren

--- nashorn/src/jdk/nashorn/internal/objects/BoundScriptFunctionImpl.java
+++ nashorn/src/jdk/nashorn/internal/objects/BoundScriptFunctionImpl.java
@@ -32,15 +32,15 @@ import jdk.nashorn.internal.runtime.ScriptRuntime;
 
 /**
  * A {@code ScriptFunctionImpl} subclass for functions created using {@code Function.prototype.bind}. Such functions
- * must track their {@code [[TargetFunction]]} property for purposes of correctly implementing {@code [[HasInstance]]};
+ * must track their {@code [[TargetFunction]] property for purposes of correctly implementing {@code [[HasInstance]]};
  * see {@link ScriptFunction#isInstance(ScriptObject)}.
  */
-final class BoundScriptFunctionImpl extends ScriptFunctionImpl {
+class BoundScriptFunctionImpl extends ScriptFunctionImpl {
     private final ScriptFunction targetFunction;
 
     BoundScriptFunctionImpl(ScriptFunctionData data, ScriptFunction targetFunction) {
-        super(data, Global.instance());
-        setPrototype(ScriptRuntime.UNDEFINED);
+        super(data);
+        this.prototype = ScriptRuntime.UNDEFINED;
         this.targetFunction = targetFunction;
     }
 
