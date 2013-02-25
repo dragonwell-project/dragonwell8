c54b346fdcbb7a5859c67b3f4daad66235d038df
Athijegannathan Sundararajan <sundar@openjdk.org>
2013-02-25 18:13:23 +0530

    8008789: Enable java access and nashorn runtime tests for jtreg
    Reviewed-by: lagergren, jlaskey, hannesw

--- nashorn/test/src/jdk/nashorn/internal/runtime/TrustedScriptEngineTest.java
+++ nashorn/test/src/jdk/nashorn/internal/runtime/TrustedScriptEngineTest.java
@@ -25,17 +25,14 @@
 
 package jdk.nashorn.internal.runtime;
 
-import static org.testng.Assert.assertEquals;
+
 import static org.testng.Assert.assertTrue;
 import static org.testng.Assert.fail;
 
 import javax.script.ScriptEngine;
 import javax.script.ScriptEngineFactory;
 import javax.script.ScriptEngineManager;
-import javax.script.ScriptContext;
 import javax.script.ScriptException;
-import javax.script.SimpleBindings;
-import javax.script.SimpleScriptContext;
 import jdk.nashorn.api.scripting.NashornScriptEngineFactory;
 import org.testng.annotations.Test;
 
@@ -43,13 +40,6 @@ import org.testng.annotations.Test;
  * Tests for trusted client usage of nashorn script engine factory extension API
  */
 public class TrustedScriptEngineTest {
-    @Test
-    public void versionTest() {
-        final ScriptEngineManager m = new ScriptEngineManager();
-        final ScriptEngine e = m.getEngineByName("nashorn");
-        assertEquals(e.getFactory().getEngineVersion(), Version.version());
-    }
-
     private static class MyClassLoader extends ClassLoader {
         // to check if script engine uses the specified class loader
         private final boolean[] reached = new boolean[1];
@@ -125,99 +115,5 @@ public class TrustedScriptEngineTest {
         }
 
         fail("Cannot find nashorn factory!");
-    }
-
-    @Test
-    public void factoryOptionsTest() {
-        final ScriptEngineManager sm = new ScriptEngineManager();
-        for (ScriptEngineFactory fac : sm.getEngineFactories()) {
-            if (fac instanceof NashornScriptEngineFactory) {
-                final NashornScriptEngineFactory nfac = (NashornScriptEngineFactory)fac;
-                // specify --no-syntax-extensions flag
-                final String[] options = new String[] { "--no-syntax-extensions" };
-                final ScriptEngine e = nfac.getScriptEngine(options);
-                try {
-                    // try nashorn specific extension
-                    e.eval("var f = funtion(x) 2*x;");
-                    fail("should have thrown exception!");
-                } catch (final ScriptException se) {
-                }
-                return;
-            }
-        }
-
-        fail("Cannot find nashorn factory!");
-    }
-
-    @Test
-    /**
-     * Test repeated evals with --loader-per-compile=false
-     * We used to get "class redefinition error".
-     */
-    public void noLoaderPerCompilerTest() {
-        final ScriptEngineManager sm = new ScriptEngineManager();
-        for (ScriptEngineFactory fac : sm.getEngineFactories()) {
-            if (fac instanceof NashornScriptEngineFactory) {
-                final NashornScriptEngineFactory nfac = (NashornScriptEngineFactory)fac;
-                final String[] options = new String[] { "--loader-per-compile=false" };
-                final ScriptEngine e = nfac.getScriptEngine(options);
-                try {
-                    e.eval("2 + 3");
-                    e.eval("4 + 4");
-                } catch (final ScriptException se) {
-                    se.printStackTrace();
-                    fail(se.getMessage());
-                }
-                return;
-            }
-        }
-        fail("Cannot find nashorn factory!");
-    }
-
-    @Test
-    /**
-     * Test that we can use same script name in repeated evals with --loader-per-compile=false
-     * We used to get "class redefinition error" as name was derived from script name.
-     */
-    public void noLoaderPerCompilerWithSameNameTest() {
-        final ScriptEngineManager sm = new ScriptEngineManager();
-        for (ScriptEngineFactory fac : sm.getEngineFactories()) {
-            if (fac instanceof NashornScriptEngineFactory) {
-                final NashornScriptEngineFactory nfac = (NashornScriptEngineFactory)fac;
-                final String[] options = new String[] { "--loader-per-compile=false" };
-                final ScriptEngine e = nfac.getScriptEngine(options);
-                e.put(ScriptEngine.FILENAME, "test.js");
-                try {
-                    e.eval("2 + 3");
-                    e.eval("4 + 4");
-                } catch (final ScriptException se) {
-                    se.printStackTrace();
-                    fail(se.getMessage());
-                }
-                return;
-            }
-        }
-        fail("Cannot find nashorn factory!");
-    }
-
-    @Test
-    public void globalPerEngineTest() throws ScriptException {
-        final NashornScriptEngineFactory fac = new NashornScriptEngineFactory();
-        final String[] options = new String[] { "--global-per-engine" };
-        final ScriptEngine e = fac.getScriptEngine(options);
-
-        e.eval("function foo() {}");
-
-        final ScriptContext newCtx = new SimpleScriptContext();
-        newCtx.setBindings(e.createBindings(), ScriptContext.ENGINE_SCOPE);
-
-        // all global definitions shared and so 'foo' should be
-        // visible in new Bindings as well.
-        assertTrue(e.eval("typeof foo", newCtx).equals("function"));
-
-        e.eval("function bar() {}", newCtx);
-
-        // bar should be visible in default context
-        assertTrue(e.eval("typeof bar").equals("function"));
     }
 }
