33bd2fae0a733b0f14f781d8c1aab12bbddcbeba
Andrew Dinn <adinn@redhat.com>
2013-05-14 10:07:34 +0100

    added notifyCompile for C1 stubs so they can be broken
    for example set a break on the div by zero handler code using
    
      simbreakreloc "throw_div0_exception_Runtime1_stub" 0
    
    and then run a method which tries to divide by zero

--- src/share/vm/c1/c1_Runtime1.cpp
+++ src/share/vm/c1/c1_Runtime1.cpp
@@ -58,6 +58,9 @@
 #include "utilities/copy.hpp"
 #include "utilities/events.hpp"
 
+#ifdef TARGET_ARCH_aarch64
+#include "../../../../../../simulator/simulator.hpp"
+#endif
 
 // Implementation of StubAssembler
 
@@ -224,6 +227,22 @@ void Runtime1::generate_blob_for(BufferBlob* buffer_blob, StubID id) {
                                                  sasm->frame_size(),
                                                  oop_maps,
                                                  sasm->must_gc_arguments());
+#ifdef TARGET_ARCH_aarch64
+  if (NotifySimulator) {
+    // tell the sim about the new stub code
+    AArch64Simulator *simulator = AArch64Simulator::current();
+    char name[400];
+    strcpy(name, name_for(id));
+    // replace spaces with underscore so we can write to file and reparse
+    for (char *p = strpbrk(name, " "); p; p = strpbrk(p, " ")) {
+      *p = '_';
+    }
+    unsigned char *base = blob->code_begin();
+    simulator->notifyCompile(name, base);
+    // code does not get relocated so just pass offset 0 and the code is live
+    simulator->notifyRelocate(base, 0);
+  }
+#endif
   // install blob
   assert(blob != NULL, "blob must exist");
   _blobs[id] = blob;
