e2c2f4cac4f593857f5b2084dfbc15a18aafc886
Andrew Dinn <adinn@redhat.com>
2013-05-23 09:51:06 +0100

    fixed lingering bug in mov_immediate64

--- src/cpu/aarch64/vm/macroAssembler_aarch64.cpp
+++ src/cpu/aarch64/vm/macroAssembler_aarch64.cpp
@@ -1063,7 +1063,7 @@ void MacroAssembler::mov_immediate64(Register dst, u_int64_t imm64)
       // one MOVN will do
       for (int i = 0; i < 4; i++) {
 	if (imm_h[i] != 0xffffL) {
-	  movn(dst, (u_int32_t)imm_h[i], (i << 4));
+	  movn(dst, (u_int32_t)imm_h[i] ^ 0xffffL, (i << 4));
 	  break;
 	}
       }
