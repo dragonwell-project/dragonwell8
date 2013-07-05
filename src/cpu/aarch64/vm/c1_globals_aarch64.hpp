3b5afa9d3c47a4cbc4488a812e89abedaebf7a16
aph <unknown>
2013-07-05 18:28:19 +0100

    Change compiler thresholds in order to exercise the compiler a bit more.

--- src/cpu/aarch64/vm/c1_globals_aarch64.hpp
+++ src/cpu/aarch64/vm/c1_globals_aarch64.hpp
@@ -40,15 +40,15 @@ define_pd_global(bool, PreferInterpreterNativeStubs, false);
 define_pd_global(bool, ProfileTraps,                 false);
 define_pd_global(bool, UseOnStackReplacement,        true );
 define_pd_global(bool, TieredCompilation,            false);
-define_pd_global(intx, CompileThreshold,             1500 );
-define_pd_global(intx, BackEdgeThreshold,            100000);
+define_pd_global(intx, CompileThreshold,             150 );
+define_pd_global(intx, BackEdgeThreshold,            500);
 
 define_pd_global(intx, OnStackReplacePercentage,     933  );
 define_pd_global(intx, FreqInlineSize,               325  );
 define_pd_global(intx, NewSizeThreadIncrease,        4*K  );
 define_pd_global(intx, InitialCodeCacheSize,         160*K);
 define_pd_global(intx, ReservedCodeCacheSize,        32*M );
-define_pd_global(bool, ProfileInterpreter,           false);
+define_pd_global(bool, ProfileInterpreter,           true);
 define_pd_global(intx, CodeCacheExpansionSize,       32*K );
 define_pd_global(uintx,CodeCacheMinBlockLength,      1);
 define_pd_global(uintx,MetaspaceSize,                     12*M );
