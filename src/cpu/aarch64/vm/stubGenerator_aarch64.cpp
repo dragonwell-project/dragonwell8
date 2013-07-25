52c4a3903f5ed6a15159630a78e48f333f0296bf
aph <unknown>
2013-07-25 18:30:12 +0100

    cmp #0; br.eq -> cbz

--- src/cpu/aarch64/vm/stubGenerator_aarch64.cpp
+++ src/cpu/aarch64/vm/stubGenerator_aarch64.cpp
@@ -276,8 +276,7 @@ class StubGenerator: public StubCodeGenerator {
     Label parameters_done;
     // parameter count is still in c_rarg6
     // and parameter pointer identifying param 1 is in c_rarg5
-    __ cmpw(c_rarg6, 0U);
-    __ br(Assembler::EQ, parameters_done);
+    __ cbz(c_rarg6, parameters_done);
 
     address loop = __ pc();
     __ ldr(rscratch1, Address(__ post(c_rarg5, wordSize)));
