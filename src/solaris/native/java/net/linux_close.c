905bcea24412bd2a50da67f6df20c93c76a004b8
chegar <unknown>
2014-03-04 13:27:55 +0000

    8036584: Review comments from 8035897 Reviewed-by: alanb

--- src/solaris/native/java/net/linux_close.c
+++ src/solaris/native/java/net/linux_close.c
@@ -36,8 +36,6 @@
 #include <errno.h>
 #include <sys/poll.h>
 
-#include "jni.h"
-
 /*
  * Stack allocated by thread when doing blocking operation
  */
@@ -326,7 +324,7 @@ int NET_Select(int s, fd_set *readfds, fd_set *writefds,
  * Auto restarts with adjusted timeout if interrupted by
  * signal other than our wakeup signal.
  */
-int NET_Timeout(JNIEnv *unused, int s, long timeout) {
+int NET_Timeout(int s, long timeout) {
     long prevtime = 0, newtime;
     struct timeval t;
     fdEntry_t *fdEntry = getFdEntry(s);
