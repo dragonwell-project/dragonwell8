0c508c8bc79009130f21be448e733d7bb28e5afc
vlivanov <unknown>
2014-06-17 13:06:05 +0400

    8046903: VM anonymous class members can't be statically invocable Reviewed-by: jrose

--- src/share/classes/java/lang/invoke/InvokerBytecodeGenerator.java
+++ src/share/classes/java/lang/invoke/InvokerBytecodeGenerator.java
@@ -39,6 +39,7 @@ import java.lang.reflect.*;
 import static java.lang.invoke.MethodHandleStatics.*;
 import static java.lang.invoke.MethodHandleNatives.Constants.*;
 import sun.invoke.util.VerifyType;
+import sun.reflect.misc.ReflectUtil;
 
 /**
  * Code generation backend for LambdaForm.
@@ -595,6 +596,8 @@ class InvokerBytecodeGenerator {
             return false;  // inner class of some sort
         if (cls.getClassLoader() != MethodHandle.class.getClassLoader())
             return false;  // not on BCP
+        if (ReflectUtil.isVMAnonymousClass(cls)) // FIXME: switch to supported API once it is added
+            return false;
         MethodType mtype = member.getMethodOrFieldType();
         if (!isStaticallyNameable(mtype.returnType()))
             return false;
@@ -613,6 +616,8 @@ class InvokerBytecodeGenerator {
             cls = cls.getComponentType();
         if (cls.isPrimitive())
             return true;  // int[].class, for example
+        if (ReflectUtil.isVMAnonymousClass(cls)) // FIXME: switch to supported API once it is added
+            return false;
         // could use VerifyAccess.isClassAccessible but the following is a safe approximation
         if (cls.getClassLoader() != Object.class.getClassLoader())
             return false;
