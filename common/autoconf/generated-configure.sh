8ea2a327ab444b522f414e552018c697a0afc9df
Abhijit Saha <asaha@openjdk.org>
2014-06-26 11:42:56 -0700

    8048270: Resolve autoconf and other merge issue from 8u20-b20 to 8u25
    Reviewed-by: katleman

--- common/autoconf/generated-configure.sh
+++ common/autoconf/generated-configure.sh
@@ -3868,7 +3868,7 @@ fi
 #CUSTOM_AUTOCONF_INCLUDE
 
 # Do not change or remove the following line, it is needed for consistency checks:
-DATE_WHEN_GENERATED=1401908461
+DATE_WHEN_GENERATED=1403797630
 
 ###############################################################################
 #
