41e80ac80d27cf36dce7ccdfb141d657b118c750
anashaty <unknown>
2014-07-08 17:47:47 +0400

    8047066: Test test/sun/awt/image/bug8038000.java fails with ClassCastException Reviewed-by: bae, prr

--- src/share/classes/sun/java2d/cmm/lcms/LCMSImageLayout.java
+++ src/share/classes/sun/java2d/cmm/lcms/LCMSImageLayout.java
@@ -383,7 +383,8 @@ class LCMSImageLayout {
     }
     public static LCMSImageLayout createImageLayout(Raster r) {
         LCMSImageLayout l = new LCMSImageLayout();
-        if (r instanceof ByteComponentRaster) {
+        if (r instanceof ByteComponentRaster &&
+                r.getSampleModel() instanceof ComponentSampleModel) {
             ByteComponentRaster br = (ByteComponentRaster)r;
 
             ComponentSampleModel csm = (ComponentSampleModel)r.getSampleModel();
