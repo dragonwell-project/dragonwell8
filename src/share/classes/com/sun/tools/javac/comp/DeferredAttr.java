126de899761b9cfed39faca38fa76fc6868c47ea
vromero <unknown>
2014-07-09 10:49:32 -0400

    8033483: Should ignore nested lambda bodies during overload resolution Reviewed-by: mcimadamore, dlsmith

--- src/share/classes/com/sun/tools/javac/comp/DeferredAttr.java
+++ src/share/classes/com/sun/tools/javac/comp/DeferredAttr.java
@@ -497,13 +497,11 @@ public class DeferredAttr extends JCTree.Visitor {
                     }
                 }
                 if (!progress) {
-                    DeferredAttrContext dac = this;
-                    while (dac != emptyDeferredAttrContext) {
-                        if (dac.mode == AttrMode.SPECULATIVE) {
-                            //unsticking does not take place during overload
-                            break;
+                    if (insideOverloadPhase()) {
+                        for (DeferredAttrNode deferredNode: deferredAttrNodes) {
+                            deferredNode.dt.tree.type = Type.noType;
                         }
-                        dac = dac.parent;
+                        return;
                     }
                     //remove all variables that have already been instantiated
                     //from the list of stuck variables
@@ -518,6 +516,17 @@ public class DeferredAttr extends JCTree.Visitor {
                     }
                 }
             }
+        }
+
+        private boolean insideOverloadPhase() {
+            DeferredAttrContext dac = this;
+            if (dac == emptyDeferredAttrContext) {
+                return false;
+            }
+            if (dac.mode == AttrMode.SPECULATIVE) {
+                return true;
+            }
+            return dac.parent.insideOverloadPhase();
         }
     }
 
@@ -579,6 +588,8 @@ public class DeferredAttr extends JCTree.Visitor {
                             return false;
                         }
                     } else {
+                        Assert.check(!deferredAttrContext.insideOverloadPhase(),
+                                "attribution shouldn't be happening here");
                         ResultInfo instResultInfo =
                                 resultInfo.dup(deferredAttrContext.inferenceContext.asInstType(resultInfo.pt));
                         dt.check(instResultInfo, dummyStuckPolicy, basicCompleter);
