701d14c9441250906bf14f83147069e33faf402f
tschatzl <unknown>
2014-12-18 09:37:02 +0100

    8067469: G1 ignores AlwaysPreTouch Summary: Factor out pretouch code of the various virtual space management classes and use them everywhere including in G1. Reviewed-by: stefank, ehelin, dholmes

--- src/share/vm/runtime/os.cpp
+++ src/share/vm/runtime/os.cpp
@@ -1513,6 +1513,11 @@ bool os::release_memory(char* addr, size_t bytes) {
   return res;
 }
 
+void os::pretouch_memory(char* start, char* end) {
+  for (volatile char *p = start; p < end; p += os::vm_page_size()) {
+    *p = 0;
+  }
+}
 
 char* os::map_memory(int fd, const char* file_name, size_t file_offset,
                            char *addr, size_t bytes, bool read_only,
