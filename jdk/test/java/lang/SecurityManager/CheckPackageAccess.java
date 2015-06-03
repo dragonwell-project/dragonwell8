2b7a1f0f83b7e8e41814cde92a22828e30efc89e
Aleksei Efimov <aefimov@openjdk.org>
2015-06-03 17:06:33 +0300

    8078427: More supportive home environment
    Reviewed-by: dfuchs, lancea, skoivu

--- jdk/test/java/lang/SecurityManager/CheckPackageAccess.java
+++ jdk/test/java/lang/SecurityManager/CheckPackageAccess.java
@@ -23,7 +23,7 @@
 
 /*
  *  @test
- *  @bug 6741606 7146431 8000450 8019830 8022945 8027144 8041633
+ *  @bug 6741606 7146431 8000450 8019830 8022945 8027144 8041633 8078427
  *  @summary Make sure all restricted packages listed in the package.access
  *           property in the java.security file are blocked
  *  @run main/othervm CheckPackageAccess
@@ -75,6 +75,7 @@ public class CheckPackageAccess {
         "com.sun.org.apache.xalan.internal.xsltc.util.",
         "com.sun.org.apache.xml.internal.res.",
         "com.sun.org.apache.xml.internal.security.",
+        "com.sun.org.apache.xml.internal.serializer.dom3.",
         "com.sun.org.apache.xml.internal.serializer.utils.",
         "com.sun.org.apache.xml.internal.utils.",
         "com.sun.org.glassfish.",
