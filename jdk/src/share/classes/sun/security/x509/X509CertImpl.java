ee0e15274daebb87ca2600c185bc640983e93945
Ivan Gerasimov <igerasim@openjdk.org>
2015-09-08 12:56:41 +0300

    8074068: Cleanup in java.base/share/classes/sun/security/x509/
    Reviewed-by: mullan, ahgross, coffeys

--- jdk/src/share/classes/sun/security/x509/X509CertImpl.java
+++ jdk/src/share/classes/sun/security/x509/X509CertImpl.java
@@ -1008,9 +1008,7 @@ public class X509CertImpl extends X509Certificate implements DerEncoder {
     public byte[] getSignature() {
         if (signature == null)
             return null;
-        byte[] dup = new byte[signature.length];
-        System.arraycopy(signature, 0, dup, 0, dup.length);
-        return dup;
+        return signature.clone();
     }
 
     /**
