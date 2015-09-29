3b3c6b059b8e1b5d7e02c78e3fe0cc18710a62b7
aph <unknown>
2015-09-29 17:01:37 +0000

    8138575: Improve generated code for profile counters Reviewed-by: kvn

--- src/cpu/aarch64/vm/macroAssembler_aarch64.hpp
+++ src/cpu/aarch64/vm/macroAssembler_aarch64.hpp
@@ -899,13 +899,7 @@ public:
 
   // Arithmetics
 
-  void addptr(Address dst, int32_t src) {
-    lea(rscratch2, dst);
-    ldr(rscratch1, Address(rscratch2));
-    add(rscratch1, rscratch1, src);
-    str(rscratch1, Address(rscratch2));
-  }
-
+  void addptr(const Address &dst, int32_t src);
   void cmpptr(Register src1, Address src2);
 
   void cmpxchgptr(Register oldv, Register newv, Register addr, Register tmp,
