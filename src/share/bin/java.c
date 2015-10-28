f18b43e7ab7eef8b39e952a2b917c0f3bf676518
ksrini <unknown>
2015-10-28 17:51:26 +0000

    8068033: JNI exception pending in jdk/src/share/bin/java.c Reviewed-by: mchung, serb

--- src/share/bin/java.c
+++ src/share/bin/java.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1995, 2014, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1995, 2015, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -459,6 +459,7 @@ JavaMain(void * _args)
      * of the application class.
      */
     PostJVMInit(env, appClass, vm);
+    CHECK_EXCEPTION_LEAVE(1);
     /*
      * The LoadMainClass not only loads the main class, it will also ensure
      * that the main method's signature is correct, therefore further checking
