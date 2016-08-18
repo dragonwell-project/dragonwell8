9205b234189602d05459f917e7bf4b9b395341df
mcherkas <unknown>
2016-08-18 19:35:50 +0300

    8164119: MoveToOtherScreenTest fails due wrong key attribute Reviewed-by: coffeys

--- test/java/awt/Container/MoveToOtherScreenTest/MoveToOtherScreenTest.java
+++ test/java/awt/Container/MoveToOtherScreenTest/MoveToOtherScreenTest.java
@@ -39,7 +39,6 @@ import java.lang.reflect.InvocationTargetException;
    @summary IllegalArgumentException: adding a component to a container on a different GraphicsDevice
    @author Mikhail Cherkasov
    @run main MoveToOtherScreenTest
-   @key headful
 */
 public class MoveToOtherScreenTest {
 
