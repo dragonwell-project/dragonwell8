214664397d0209a782aa15f4a8b2f0099007cd0c
Mikhail Cherkasov <mcherkas@openjdk.org>
2017-01-24 22:39:49 +0300

    8167110: Windows peering issue 7155957: closed/java/awt/MenuBar/MenuBarStress1/MenuBarStress1.java hangs on win 64 bit with jdk8 8079595: Resizing dialog which is JWindow parent makes JVM crash 8147842: IME Composition Window is displayed at incorrect location
    Reviewed-by: serb

--- jdk/src/windows/classes/sun/awt/windows/WObjectPeer.java
+++ jdk/src/windows/classes/sun/awt/windows/WObjectPeer.java
@@ -34,16 +34,16 @@ abstract class WObjectPeer {
     }
 
     // The Windows handle for the native widget.
-    long pData;
+    volatile long pData;
     // if the native peer has been destroyed
-    boolean destroyed = false;
+    private volatile boolean destroyed;
     // The associated AWT object.
-    Object target;
+    volatile Object target;
 
     private volatile boolean disposed;
 
     // set from JNI if any errors in creating the peer occur
-    protected Error createError = null;
+    volatile Error createError = null;
 
     // used to synchronize the state of this peer
     private final Object stateLock = new Object();
