59fd7aef155643f459e55724d6d11da9f9b6c0ce
Phil Race <prr@openjdk.org>
2017-04-07 17:54:06 +0530

    8171262: Stability fixes for lcms
    Reviewed-by: serb, vadim, mschoene

--- jdk/src/share/native/sun/java2d/cmm/lcms/cmstypes.c
+++ jdk/src/share/native/sun/java2d/cmm/lcms/cmstypes.c
@@ -4452,7 +4452,8 @@ void *Type_MPE_Read(struct _cms_typehandler_struct* self, cmsIOHANDLER* io, cmsU
     NewLUT = cmsPipelineAlloc(self ->ContextID, InputChans, OutputChans);
     if (NewLUT == NULL) return NULL;
 
-    if (!_cmsReadUInt32Number(io, &ElementCount)) return NULL;
+    if (!_cmsReadUInt32Number(io, &ElementCount)) goto Error;
+    if (!ReadPositionTable(self, io, ElementCount, BaseOffset, NewLUT, ReadMPEElem)) goto Error;
 
     if (!ReadPositionTable(self, io, ElementCount, BaseOffset, NewLUT, ReadMPEElem)) {
         if (NewLUT != NULL) cmsPipelineFree(NewLUT);
@@ -4463,6 +4464,12 @@ void *Type_MPE_Read(struct _cms_typehandler_struct* self, cmsIOHANDLER* io, cmsU
     // Success
     *nItems = 1;
     return NewLUT;
+
+    // Error
+Error:
+    if (NewLUT != NULL) cmsPipelineFree(NewLUT);
+    *nItems = 0;
+    return NULL;
 
     cmsUNUSED_PARAMETER(SizeOfTag);
 }
