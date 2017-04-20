deecb14bb8bbefc7b7eaa7ea7e330d769a3bc376
Sergey Bylokhov <serb@openjdk.org>
2017-04-20 10:31:17 +0530

    8169966: Larger AWT menus
    Reviewed-by: azvegint, prr, rhalade, mschoene

--- jdk/src/windows/native/sun/windows/awt_Toolkit.cpp
+++ jdk/src/windows/native/sun/windows/awt_Toolkit.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1996, 2014, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1996, 2017, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -1581,6 +1581,11 @@ void AwtToolkit::SyncCall(void (*ftn)(void)) {
     } else {
         (*ftn)();
     }
+}
+
+jboolean AwtToolkit::isFreeIDAvailable()
+{
+    return m_cmdIDs->isFreeIDAvailable();
 }
 
 UINT AwtToolkit::CreateCmdID(AwtObject* object)
