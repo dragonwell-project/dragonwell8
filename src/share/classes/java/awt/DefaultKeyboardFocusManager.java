aa66992c0a699e0dacc51fedf0f236637ef7488f
dmarkov <unknown>
2017-10-11 10:03:16 +0100

    8155197: Focus transition issue Reviewed-by: serb, ssadetsky, aivanov

--- src/share/classes/java/awt/DefaultKeyboardFocusManager.java
+++ src/share/classes/java/awt/DefaultKeyboardFocusManager.java
@@ -181,11 +181,16 @@ public class DefaultKeyboardFocusManager extends KeyboardFocusManager {
     private boolean doRestoreFocus(Component toFocus, Component vetoedComponent,
                                    boolean clearOnFailure)
     {
+        boolean success = true;
         if (toFocus != vetoedComponent && toFocus.isShowing() && toFocus.canBeFocusOwner() &&
-            toFocus.requestFocus(false, CausedFocusEvent.Cause.ROLLBACK))
+            (success = toFocus.requestFocus(false, CausedFocusEvent.Cause.ROLLBACK)))
         {
             return true;
         } else {
+            if (!success && getGlobalFocusedWindow() != SunToolkit.getContainingWindow(toFocus)) {
+                restoreFocusTo = toFocus;
+                return true;
+            }
             Component nextFocus = toFocus.getNextFocusCandidate();
             if (nextFocus != null && nextFocus != vetoedComponent &&
                 nextFocus.requestFocusInWindow(CausedFocusEvent.Cause.ROLLBACK))
