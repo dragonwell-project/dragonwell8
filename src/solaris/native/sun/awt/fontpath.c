40ba05665372c52827e01f166a06c9a635108ff7
prr <unknown>
2017-10-25 13:11:07 -0700

    8170681: Remove fontconfig header files from JDK source tree Reviewed-by: serb, erikj

--- src/solaris/native/sun/awt/fontpath.c
+++ src/solaris/native/sun/awt/fontpath.c
@@ -571,7 +571,7 @@ JNIEXPORT jstring JNICALL Java_sun_awt_FcFontManager_getFontPathNative
 
 #include <dlfcn.h>
 
-#include "fontconfig.h"
+#include <fontconfig/fontconfig.h>
 
 
 static void* openFontConfig() {
