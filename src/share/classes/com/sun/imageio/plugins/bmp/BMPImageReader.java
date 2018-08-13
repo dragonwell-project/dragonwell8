7b1e9dbfab86372306a8197487176a58dc3b6b0e
fmatte <unknown>
2018-08-13 10:47:07 -0400

    8182461: IndexOutOfBoundsException when reading indexed color BMP Summary: added missing break to prevent erroneous fall through Reviewed-by: prr, serb

--- src/share/classes/com/sun/imageio/plugins/bmp/BMPImageReader.java
+++ src/share/classes/com/sun/imageio/plugins/bmp/BMPImageReader.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2003, 2013, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2003, 2018, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -915,6 +915,7 @@ public class BMPImageReader extends ImageReader implements BMPConstants {
                 throw new
                     IIOException(I18N.getString("BMPImageReader1"));
             }
+            break;
 
         case VERSION_4_8_BIT:
             switch((int)compression) {
