d97ba67c293f56f70a3f43ced340c13d521ec7cf
Dmitry Markov <dmarkov@openjdk.org>
2018-08-17 15:53:41 +0100

    8205479: OS X: requestFocus() does not work properly for embedded frame
    Reviewed-by: serb, aivanov

--- jdk/src/macosx/classes/sun/lwawt/macosx/CPlatformEmbeddedFrame.java
+++ jdk/src/macosx/classes/sun/lwawt/macosx/CPlatformEmbeddedFrame.java
@@ -146,6 +146,8 @@ public class CPlatformEmbeddedFrame implements PlatformWindow {
 
     @Override
     public boolean requestWindowFocus() {
+        CEmbeddedFrame.updateGlobalFocusedWindow(target);
+        target.synthesizeWindowActivation(true);
         return true;
     }
 
