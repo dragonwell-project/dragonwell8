5b3ebe4396b86d3fd632e8e8fae08a1cb239174c
robm <unknown>
2018-10-05 14:05:02 -0700

    8202261: (fc) FileChannel.map and RandomAccessFile.setLength should not preallocate space Reviewed-by: coffeys

--- src/windows/classes/sun/nio/ch/FileDispatcherImpl.java
+++ src/windows/classes/sun/nio/ch/FileDispatcherImpl.java
@@ -85,16 +85,15 @@ class FileDispatcherImpl extends FileDispatcher {
         return writev0(fd, address, len, append);
     }
 
+    long seek(FileDescriptor fd, long offset) throws IOException {
+        return seek0(fd, offset);
+    }
+
     int force(FileDescriptor fd, boolean metaData) throws IOException {
         return force0(fd, metaData);
     }
 
     int truncate(FileDescriptor fd, long size) throws IOException {
-        return truncate0(fd, size);
-    }
-
-    int allocate(FileDescriptor fd, long size) throws IOException {
-        // truncate0() works for extending and truncating file size
         return truncate0(fd, size);
     }
 
@@ -175,6 +174,8 @@ class FileDispatcherImpl extends FileDispatcher {
 
     static native long writev0(FileDescriptor fd, long address, int len, boolean append)
         throws IOException;
+
+    static native long seek0(FileDescriptor fd, long offset) throws IOException;
 
     static native int force0(FileDescriptor fd, boolean metaData)
         throws IOException;
