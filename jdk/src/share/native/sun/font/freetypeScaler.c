049c482e43fbfa176cdf5c5e3525d2792ffa107a
Ichiroh Takiguchi <itakiguchi@openjdk.org>
2018-12-05 12:59:50 -0800

    8214002: Cannot use italic font style if the font has embedded bitmap
    Reviewed-by: prr

--- jdk/src/share/native/sun/font/freetypeScaler.c
+++ jdk/src/share/native/sun/font/freetypeScaler.c
@@ -680,7 +680,7 @@ Java_sun_font_FreetypeFontScaler_getGlyphImageNative(
     UInt16 width, height;
     GlyphInfo *glyphInfo;
     int glyph_index;
-    int renderFlags = FT_LOAD_RENDER, target;
+    int renderFlags = FT_LOAD_DEFAULT, target;
     FT_GlyphSlot ftglyph;
 
     FTScalerContext* context =
