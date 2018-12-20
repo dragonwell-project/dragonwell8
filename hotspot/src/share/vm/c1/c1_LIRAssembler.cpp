6a331c71838e7ee65773853c3b227eece83b5d34
Paul Hohensee <phh@openjdk.org>
2018-12-20 20:11:38 +0000

    8206075: On x86, assert on unbound assembler Labels used as branch targets 8208480: Test failure: assert(is_bound() || is_unused()) after JDK-8206075 in C1
    Combine unbound Label assertion checking backports
    
    Reviewed-by: andrew, coffeys

--- hotspot/src/share/vm/c1/c1_LIRAssembler.cpp
+++ hotspot/src/share/vm/c1/c1_LIRAssembler.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2000, 2013, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2000, 2018, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -128,6 +128,9 @@ LIR_Assembler::LIR_Assembler(Compilation* c):
 
 
 LIR_Assembler::~LIR_Assembler() {
+  // The unwind handler label may be unbound if this destructor is invoked because of a bail-out.
+  // Reset it here to avoid an assertion.
+  _unwind_handler_entry.reset();
 }
 
 
