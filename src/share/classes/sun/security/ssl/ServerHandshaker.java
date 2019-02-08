416bba20c2446554fccbc7860d0eaa2f864b0c00
xuelei <unknown>
2019-02-08 10:03:07 -0800

    8218580: endpoint identification algorithm should be case-insensitive Reviewed-by: jnimeh Contributed-by: verghese@amazon.com

--- src/share/classes/sun/security/ssl/ServerHandshaker.java
+++ src/share/classes/sun/security/ssl/ServerHandshaker.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1996, 2018, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1996, 2019, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -705,7 +705,7 @@ final class ServerHandshaker extends Handshaker {
 
                     String sessionIdentityAlg =
                     previous.getEndpointIdentificationAlgorithm();
-                    if (!Objects.equals(identityAlg, sessionIdentityAlg)) {
+                    if (!identityAlg.equalsIgnoreCase(sessionIdentityAlg)) {
 
                         if (debug != null && Debug.isOn("session")) {
                             System.out.println("%% can't resume, endpoint id"
