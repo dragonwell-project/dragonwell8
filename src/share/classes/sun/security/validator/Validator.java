6cc3f9d3bf1d867908dc4c10df408064a1d4a2e0
mullan <unknown>
2019-03-01 00:55:45 +0000

    8207258: Distrust TLS server certificates anchored by Symantec Root CAs Summary: Add class CADistrustPolicy and distrust Symantec. Reviewed-by: weijun

--- src/share/classes/sun/security/validator/Validator.java
+++ src/share/classes/sun/security/validator/Validator.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2002, 2015, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2002, 2019, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -271,7 +271,7 @@ public abstract class Validator {
             // redundant.
             boolean checkUnresolvedCritExts =
                     (type == TYPE_PKIX) ? false : true;
-            endEntityChecker.check(chain[0], parameter,
+            endEntityChecker.check(chain, parameter,
                                    checkUnresolvedCritExts);
         }
 
