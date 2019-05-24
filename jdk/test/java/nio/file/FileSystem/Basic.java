bad01470d7b253762929a6c69166f75fa5bcd0ae
Andrew Dinn <adinn@openjdk.org>
2019-05-24 16:15:32 +0100

    8176237: (fs) java/nio/file/FileStore/Basic.java should conditionally check FileStores
    On Unix platforms, spawn a 'df' process and skip FileStore check if it hangs
    
    Reviewed-by: alanb, chegar

--- jdk/test/java/nio/file/FileSystem/Basic.java
+++ jdk/test/java/nio/file/FileSystem/Basic.java
@@ -24,7 +24,9 @@
 /* @test
  * @bug 4313887 6838333
  * @summary Unit test for java.nio.file.FileSystem
- * @library ..
+ * @library .. /lib/testlibrary
+ * @build jdk.testlibrary.FileUtils
+ * @run main Basic
  */
 
 import java.io.File;
@@ -40,6 +42,7 @@ import java.nio.file.Paths;
 import java.nio.file.ProviderNotFoundException;
 import java.util.HashMap;
 import java.util.concurrent.TimeUnit;
+import jdk.testlibrary.FileUtils;
 
 /**
  * Simple santity checks for java.nio.file.FileSystem
@@ -51,41 +54,17 @@ public class Basic {
             throw new RuntimeException(msg);
     }
 
-    static void checkFileStores(String os, FileSystem fs) throws IOException {
-        boolean checkFileStores = true;
-        if (!os.equals("Windows")) {
-            // try to check whether 'df' hangs
-            System.out.println("\n--- Begin df output ---");
-            System.out.flush();
-            Process proc = new ProcessBuilder("df").inheritIO().start();
-            try {
-                proc.waitFor(90, TimeUnit.SECONDS);
-            } catch (InterruptedException ignored) {
-            }
-            System.out.println("--- End df output ---\n");
-            System.out.flush();
-            try {
-                int exitValue = proc.exitValue();
-                if (exitValue != 0) {
-                    System.err.printf("df process exited with %d != 0%n",
-                        exitValue);
-                    checkFileStores = false;
-                }
-            } catch (IllegalThreadStateException ignored) {
-                System.err.println("df command apparently hung");
-                checkFileStores = false;
-            }
-        }
-
+    static void checkFileStores(FileSystem fs) throws IOException {
         // sanity check method
-        if (checkFileStores) {
+        if (FileUtils.areFileSystemsAccessible()) {
             System.out.println("\n--- Begin FileStores ---");
             for (FileStore store: fs.getFileStores()) {
                 System.out.println(store);
             }
             System.out.println("--- EndFileStores ---\n");
         } else {
-            System.err.println("Skipping FileStore check due to df failure");
+            System.err.println
+                ("Skipping FileStore check due to file system access failure");
         }
     }
 
@@ -114,7 +93,7 @@ public class Basic {
             "should use 'file' scheme");
 
         // sanity check FileStores
-        checkFileStores(os, fs);
+        checkFileStores(fs);
 
         // sanity check supportedFileAttributeViews
         checkSupported(fs, "basic");
