89ffa7bc2f0ea2bc7eeca73c8e86f72bcfa93cf4
aph <unknown>
2019-05-28 09:03:15 -0400

    8224671: AArch64: mauve System.arraycopy test failure Reviewed-by: adinn

--- src/cpu/aarch64/vm/c1_LIRAssembler_aarch64.cpp
+++ src/cpu/aarch64/vm/c1_LIRAssembler_aarch64.cpp
@@ -2266,7 +2266,7 @@ void LIR_Assembler::emit_arraycopy(LIR_OpArrayCopy* op) {
 
     if (copyfunc_addr != NULL) {
       // r0 is -1^K where K == partial copied count
-      __ eonw(rscratch1, r0, 0);
+      __ eonw(rscratch1, r0, zr);
       // adjust length down and src/end pos up by partial copied count
       __ subw(length, length, rscratch1);
       __ addw(src_pos, src_pos, rscratch1);
