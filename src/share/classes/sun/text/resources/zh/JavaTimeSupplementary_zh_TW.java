9baca700d04af084f52fe97355cc9e9bbfab6c90
andrew <unknown>
2019-07-01 20:26:04 +0100

    8219781: Localized names for Japanese era Reiwa in COMPAT provider Reviewed-by: naoto, aph, phh

--- src/share/classes/sun/text/resources/zh/JavaTimeSupplementary_zh_TW.java
+++ src/share/classes/sun/text/resources/zh/JavaTimeSupplementary_zh_TW.java
@@ -175,6 +175,7 @@ public class JavaTimeSupplementary_zh_TW extends OpenListResourceBundle {
                     "\u5927\u6b63",
                     "\u662d\u548c",
                     "\u5e73\u6210",
+                    "\u4ee4\u548c",
                 }
             },
             { "java.time.japanese.short.Eras",
@@ -184,6 +185,7 @@ public class JavaTimeSupplementary_zh_TW extends OpenListResourceBundle {
                     "\u5927\u6b63",
                     "\u662d\u548c",
                     "\u5e73\u6210",
+                    "\u4ee4\u548c",
                 }
             },
             { "java.time.roc.DatePatterns",
