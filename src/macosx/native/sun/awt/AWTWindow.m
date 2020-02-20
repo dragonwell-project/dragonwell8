5bdba4737403aa3b17d905a3d48cb9a682af19b4
stooke <unknown>
2020-02-20 06:04:27 +0000

    8215756: Memory leaks in the AWT on macOS Reviewed-by: shade, andrew

--- src/macosx/native/sun/awt/AWTWindow.m
+++ src/macosx/native/sun/awt/AWTWindow.m
@@ -456,7 +456,7 @@ AWT_ASSERT_APPKIT_THREAD;
 
     JNIEnv *env = [ThreadUtilities getJNIEnvUncached];
     [self.javaPlatformWindow setJObject:nil withEnv:env];
-
+    self.javaPlatformWindow = nil;
     self.nsWindow = nil;
     self.ownerWindow = nil;
     [super dealloc];
