37862f19861c8618281da2377281411c61cb733f
aph <unknown>
2020-04-03 14:14:26 +0100

    8076475: Misuses of strncpy/strncat Summary: Various small fixes around strncpy and strncat Reviewed-by: andrew

--- src/share/tools/hsdis/hsdis.c
+++ src/share/tools/hsdis/hsdis.c
@@ -438,6 +438,7 @@ static void parse_caller_options(struct hsdis_app_data* app_data, const char* ca
     }
     p = q;
   }
+  *iop = '\0';
 }
 
 static void print_help(struct hsdis_app_data* app_data,
