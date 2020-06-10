403a33587bdb95fd9bd2282df96342497cc91146
Goetz Lindenmaier <goetz@openjdk.org>
2020-06-10 11:33:17 +0200

    8244225: stringop-overflow warning on strncpy call from compile_the_world_in
    Reviewed-by: aph, shade, andrew

--- hotspot/src/share/vm/classfile/classLoader.cpp
+++ hotspot/src/share/vm/classfile/classLoader.cpp
@@ -1635,12 +1635,13 @@ static bool can_be_compiled(methodHandle m, int comp_level) {
 }
 
 void ClassLoader::compile_the_world_in(char* name, Handle loader, TRAPS) {
-  int len = (int)strlen(name);
+  size_t len = strlen(name);
   if (len > 6 && strcmp(".class", name + len - 6) == 0) {
     // We have a .class file
     char buffer[2048];
-    strncpy(buffer, name, len - 6);
-    buffer[len-6] = 0;
+    if (len-6 >= sizeof(buffer)) return;
+    strncpy(buffer, name, sizeof(buffer));
+    buffer[len-6] = 0; // Truncate ".class" suffix.
     // If the file has a period after removing .class, it's not really a
     // valid class file.  The class loader will check everything else.
     if (strchr(buffer, '.') == NULL) {
