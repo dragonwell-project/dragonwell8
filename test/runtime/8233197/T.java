6a741c20699cb0aef78a7fe02ce0220c11b9d794
jbachorik <unknown>
2020-06-12 02:59:56 +0100

    8233197: Invert JvmtiExport::post_vm_initialized() and Jfr:on_vm_start() start-up order for correct option parsing 8246703: [TESTBUG] Add test for JDK-8233197 Reviewed-by: aph, adinn, neugens

---
+++ test/runtime/8233197/T.java
@@ -0,0 +1,11 @@
+public class T
+{
+  public static void main(String[] args) throws Exception
+  {
+    for (int i = 0; i < 50; i++) {
+      System.out.print("+");
+      Thread.sleep(1);
+    }
+    System.out.println();
+  }
+}
