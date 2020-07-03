bee2bfea3740ed75d43278052c0ddb7d3bae9769
sgehwolf <unknown>
2020-07-03 15:09:27 +0200

    8226575: OperatingSystemMXBean should be made container aware Reviewed-by: andrew

--- src/solaris/classes/sun/management/OperatingSystemImpl.java
+++ src/solaris/classes/sun/management/OperatingSystemImpl.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2003, 2013, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2003, 2019, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -25,6 +25,9 @@
 
 package sun.management;
 
+import jdk.internal.platform.Metrics;
+import java.util.concurrent.TimeUnit;
+
 /**
  * Implementation class for the operating system.
  * Standard and committed hotspot-specific metrics if any.
@@ -35,20 +38,134 @@ package sun.management;
 class OperatingSystemImpl extends BaseOperatingSystemImpl
     implements com.sun.management.UnixOperatingSystemMXBean {
 
+    private static final int MAX_ATTEMPTS_NUMBER = 10;
+    private final Metrics containerMetrics;
+
     OperatingSystemImpl(VMManagement vm) {
         super(vm);
+        this.containerMetrics = jdk.internal.platform.Container.metrics();
+    }
+
+    public long getTotalSwapSpaceSize() {
+        if (containerMetrics != null) {
+            long limit = containerMetrics.getMemoryAndSwapLimit();
+            // The memory limit metrics is not available if JVM runs on Linux host (not in a docker container)
+            // or if a docker container was started without specifying a memory limit (without '--memory='
+            // Docker option). In latter case there is no limit on how much memory the container can use and
+            // it can use as much memory as the host's OS allows.
+            long memLimit = containerMetrics.getMemoryLimit();
+            if (limit >= 0 && memLimit >= 0) {
+                return limit - memLimit;
+            }
+        }
+        return getTotalSwapSpaceSize0();
+    }
+
+    public long getFreeSwapSpaceSize() {
+        if (containerMetrics != null) {
+            long memSwapLimit = containerMetrics.getMemoryAndSwapLimit();
+            long memLimit = containerMetrics.getMemoryLimit();
+            if (memSwapLimit >= 0 && memLimit >= 0) {
+                for (int attempt = 0; attempt < MAX_ATTEMPTS_NUMBER; attempt++) {
+                    long memSwapUsage = containerMetrics.getMemoryAndSwapUsage();
+                    long memUsage = containerMetrics.getMemoryUsage();
+                    if (memSwapUsage > 0 && memUsage > 0) {
+                        // We read "memory usage" and "memory and swap usage" not atomically,
+                        // and it's possible to get the negative value when subtracting these two.
+                        // If this happens just retry the loop for a few iterations.
+                        if ((memSwapUsage - memUsage) >= 0) {
+                            return memSwapLimit - memLimit - (memSwapUsage - memUsage);
+                        }
+                    }
+                }
+            }
+        }
+        return getFreeSwapSpaceSize0();
+    }
+
+    public long getFreePhysicalMemorySize() {
+        if (containerMetrics != null) {
+            long usage = containerMetrics.getMemoryUsage();
+            long limit = containerMetrics.getMemoryLimit();
+            if (usage > 0 && limit >= 0) {
+                return limit - usage;
+            }
+        }
+        return getFreePhysicalMemorySize0();
+    }
+
+    public long getTotalPhysicalMemorySize() {
+        if (containerMetrics != null) {
+            long limit = containerMetrics.getMemoryLimit();
+            if (limit >= 0) {
+                return limit;
+            }
+        }
+        return getTotalPhysicalMemorySize0();
+    }
+
+    public double getSystemCpuLoad() {
+        if (containerMetrics != null) {
+            long quota = containerMetrics.getCpuQuota();
+            if (quota > 0) {
+                long periodLength = containerMetrics.getCpuPeriod();
+                long numPeriods = containerMetrics.getCpuNumPeriods();
+                long usageNanos = containerMetrics.getCpuUsage();
+                if (periodLength > 0 && numPeriods > 0 && usageNanos > 0) {
+                    long elapsedNanos = TimeUnit.MICROSECONDS.toNanos(periodLength * numPeriods);
+                    double systemLoad = (double) usageNanos / elapsedNanos;
+                    // Ensure the return value is in the range 0.0 -> 1.0
+                    systemLoad = Math.max(0.0, systemLoad);
+                    systemLoad = Math.min(1.0, systemLoad);
+                    return systemLoad;
+                }
+                return -1;
+            } else {
+                // If CPU quotas are not active then find the average system load for
+                // all online CPUs that are allowed to run this container.
+
+                // If the cpuset is the same as the host's one there is no need to iterate over each CPU
+                if (isCpuSetSameAsHostCpuSet()) {
+                    return getSystemCpuLoad0();
+                } else {
+                    int[] cpuSet = containerMetrics.getEffectiveCpuSetCpus();
+                    if (cpuSet != null && cpuSet.length > 0) {
+                        double systemLoad = 0.0;
+                        for (int cpu : cpuSet) {
+                            double cpuLoad = getSingleCpuLoad0(cpu);
+                            if (cpuLoad < 0) {
+                                return -1;
+                            }
+                            systemLoad += cpuLoad;
+                        }
+                        return systemLoad / cpuSet.length;
+                    }
+                    return -1;
+                }
+            }
+        }
+        return getSystemCpuLoad0();
+    }
+
+    private boolean isCpuSetSameAsHostCpuSet() {
+        if (containerMetrics != null) {
+            return containerMetrics.getCpuSetCpus().length == getHostConfiguredCpuCount0();
+        }
+        return false;
     }
 
     public native long getCommittedVirtualMemorySize();
-    public native long getTotalSwapSpaceSize();
-    public native long getFreeSwapSpaceSize();
+    private native long getTotalSwapSpaceSize0();
+    private native long getFreeSwapSpaceSize0();
     public native long getProcessCpuTime();
-    public native long getFreePhysicalMemorySize();
-    public native long getTotalPhysicalMemorySize();
+    private native long getFreePhysicalMemorySize0();
+    private native long getTotalPhysicalMemorySize0();
     public native long getOpenFileDescriptorCount();
     public native long getMaxFileDescriptorCount();
-    public native double getSystemCpuLoad();
+    private native double getSystemCpuLoad0();
     public native double getProcessCpuLoad();
+    private native double getSingleCpuLoad0(int cpuNum);
+    private native int getHostConfiguredCpuCount0();
 
     static {
         initialize();
