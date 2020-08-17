caa73fb59a59cc7beca1680d9044b2b9765e1eba
Sergey Bylokhov <serb@openjdk.org>
2020-08-18 00:06:57 +0100

    8022535: [TEST BUG] javax/swing/text/html/parser/Test8017492.java fails
    Reviewed-by: prr, pbansal

--- jdk/test/javax/swing/text/html/parser/Test8017492.java
+++ jdk/test/javax/swing/text/html/parser/Test8017492.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2013, 2020, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -70,6 +70,7 @@ public class Test8017492 {
         thread.start();
         thread.join();
         // add error handling
+        SunToolkit.createNewAppContext();
         HTMLDocument document = new HTMLDocument() {
             @Override
             public HTMLEditorKit.ParserCallback getReader(int pos) {
