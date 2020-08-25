c5338c69e705d450d11df94296b864b29f65d319
abakhtin <unknown>
2020-08-25 18:03:27 +0300

    8245653: Remove 8u TLS tests Reviewed-by: mbalao

--- test/sun/security/ssl/HandshakeOutStream/NullCerts.java
+++ test/sun/security/ssl/HandshakeOutStream/NullCerts.java
@@ -56,7 +56,6 @@ public class NullCerts {
     /*
      * Where do we find the keystores?
      */
-    // private final static String pathToStores = "./etc";
     private final static String pathToStores = "../../../../javax/net/ssl/etc";
     private final static String keyStoreFile = "keystore";
     private final static String trustStoreFile = "truststore";
