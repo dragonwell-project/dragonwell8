9ad7ddaa16da5ee7e8993d422e9e8248f4b6a673
Yuri Nesterenko <yan@openjdk.org>
2020-10-17 02:55:07 +0100

    8245417: Improve certificate chain handling
    Reviewed-by: mbalao, andrew

--- jdk/src/share/classes/sun/security/ssl/SSLSocketInputRecord.java
+++ jdk/src/share/classes/sun/security/ssl/SSLSocketInputRecord.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1996, 2018, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1996, 2020, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -305,6 +305,15 @@ final class SSLSocketInputRecord extends InputRecord implements SSLRecord {
                 // skip the first byte: handshake type
                 byte handshakeType = handshakeFrag.get();
                 int handshakeBodyLen = Record.getInt24(handshakeFrag);
+                if (handshakeBodyLen > SSLConfiguration.maxHandshakeMessageSize) {
+                    throw new SSLProtocolException(
+                            "The size of the handshake message ("
+                            + handshakeBodyLen
+                            + ") exceeds the maximum allowed size ("
+                            + SSLConfiguration.maxHandshakeMessageSize
+                            + ")");
+                }
+
                 handshakeFrag.reset();
                 int handshakeMessageLen =
                         handshakeHeaderSize + handshakeBodyLen;
