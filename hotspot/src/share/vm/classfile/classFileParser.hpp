53141b8e7445f0afc5d5fee92e11b7d5dfb3fb86
Matthias Baesken <mbaesken@openjdk.org>
2021-01-27 07:57:32 +0000

    8260236: better init AnnotationCollector _contended_group
    Reviewed-by: coleenp, shade

--- hotspot/src/share/vm/classfile/classFileParser.hpp
+++ hotspot/src/share/vm/classfile/classFileParser.hpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1997, 2014, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1997, 2021, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -139,7 +139,7 @@ class ClassFileParser VALUE_OBJ_CLASS_SPEC {
     u2 _contended_group;
 
     AnnotationCollector(Location location)
-    : _location(location), _annotations_present(0)
+    : _location(location), _annotations_present(0), _contended_group(0)
     {
       assert((int)_annotation_LIMIT <= (int)sizeof(_annotations_present) * BitsPerByte, "");
     }
