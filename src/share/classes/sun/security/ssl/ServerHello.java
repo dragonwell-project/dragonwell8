fa68b4890bdf8f15dfaca51940b8652f47ce597d
abakhtin <unknown>
2021-05-10 09:45:35 +0000

    8241248: NullPointerException in sun.security.ssl.HKDF.extract(HKDF.java:93) Reviewed-by: jnimeh, xuelei, mdoerr

--- src/share/classes/sun/security/ssl/ServerHello.java
+++ src/share/classes/sun/security/ssl/ServerHello.java
@@ -547,11 +547,6 @@ final class ServerHello {
 
                 setUpPskKD(shc,
                         shc.resumingSession.consumePreSharedKey());
-
-                // The session can't be resumed again---remove it from cache
-                SSLSessionContextImpl sessionCache = (SSLSessionContextImpl)
-                    shc.sslContext.engineGetServerSessionContext();
-                sessionCache.remove(shc.resumingSession.getSessionId());
             }
 
             // update the responders
