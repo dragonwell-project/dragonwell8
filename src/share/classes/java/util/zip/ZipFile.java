3f01bcc5f00824714e7f6774e2336b3232431cf0
robm <unknown>
2021-05-24 11:44:14 -0400

    8262967: Improve Zip file support Reviewed-by: ahgross, rhalade, aefimov, andrew

--- src/share/classes/java/util/zip/ZipFile.java
+++ src/share/classes/java/util/zip/ZipFile.java
@@ -213,6 +213,7 @@ class ZipFile implements ZipConstants, Closeable {
                                                Integer.toHexString(mode));
         }
         String name = file.getPath();
+        file = new File(name);
         SecurityManager sm = System.getSecurityManager();
         if (sm != null) {
             sm.checkRead(name);
