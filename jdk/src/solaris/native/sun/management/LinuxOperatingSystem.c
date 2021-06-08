6235a033007f6c867c21336257adf35f3e1108b8
Severin Gehwolf <sgehwolf@openjdk.org>
2021-06-08 08:13:33 +0000

    8247469: getSystemCpuLoad() returns -1 on linux when some offline cpus are present and cpusets.effective_cpus is not available
    Reviewed-by: adinn, andrew

--- jdk/src/solaris/native/sun/management/LinuxOperatingSystem.c
+++ jdk/src/solaris/native/sun/management/LinuxOperatingSystem.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2011, 2019, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2011, 2020, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -364,4 +364,15 @@ Java_sun_management_OperatingSystemImpl_getHostConfiguredCpuCount0
     } else {
        return -1;
     }
+}
+
+JNIEXPORT jint JNICALL
+Java_sun_management_OperatingSystemImpl_getHostOnlineCpuCount0
+(JNIEnv *env, jobject mbean)
+{
+    int n = sysconf(_SC_NPROCESSORS_ONLN);
+    if (n <= 0) {
+        n = 1;
+    }
+    return n;
 }
