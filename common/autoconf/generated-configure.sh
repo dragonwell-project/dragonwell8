88ec52ff3bfb7a42a2b237c207b9fdbcbce04f04
Aleksey Shipilev <shade@openjdk.org>
2021-07-06 16:37:30 +0200

    8265978: make test should look for more locations when searching for exit code
    Reviewed-by: sgehwolf

--- common/autoconf/generated-configure.sh
+++ common/autoconf/generated-configure.sh
@@ -4399,7 +4399,7 @@ VS_SDK_PLATFORM_NAME_2017=
 #CUSTOM_AUTOCONF_INCLUDE
 
 # Do not change or remove the following line, it is needed for consistency checks:
-DATE_WHEN_GENERATED=1625577583
+DATE_WHEN_GENERATED=1625580632
 
 ###############################################################################
 #
