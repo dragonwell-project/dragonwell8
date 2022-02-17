399ad9ed3f097966da84e02415371ed22406e906
Aleksei Voitylov <avoitylov@openjdk.org>
2022-02-18 00:39:20 +0300

    8277227: Better identification of OIDs
    Reviewed-by: andrew

--- jdk/src/share/classes/sun/security/util/ObjectIdentifier.java
+++ jdk/src/share/classes/sun/security/util/ObjectIdentifier.java
@@ -443,7 +443,7 @@ class ObjectIdentifier implements Serializable
             if ((encoding[i] & 0x80) == 0) {
                 // one section [fromPos..i]
                 if (i - fromPos + 1 > 4) {
-                    BigInteger big = new BigInteger(pack(encoding, fromPos, i-fromPos+1, 7, 8));
+                    BigInteger big = new BigInteger(1, pack(encoding, fromPos, i-fromPos+1, 7, 8));
                     if (fromPos == 0) {
                         result[which++] = 2;
                         BigInteger second = big.subtract(BigInteger.valueOf(80));
@@ -508,7 +508,7 @@ class ObjectIdentifier implements Serializable
                         sb.append('.');
                     }
                     if (i - fromPos + 1 > 4) { // maybe big integer
-                        BigInteger big = new BigInteger(pack(encoding, fromPos, i-fromPos+1, 7, 8));
+                        BigInteger big = new BigInteger(1, pack(encoding, fromPos, i-fromPos+1, 7, 8));
                         if (fromPos == 0) {
                             // first section encoded with more than 4 bytes,
                             // must be 2.something
@@ -745,6 +745,7 @@ class ObjectIdentifier implements Serializable
             throw new IOException("ObjectIdentifier encoded length was " +
                     "negative: " + oidLength);
         }
+
         if (oidLength > MAXIMUM_OID_SIZE) {
             throw new IOException(
                     "ObjectIdentifier encoded length exceeds " +
