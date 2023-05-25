20b4c702073a20e7809024277cb03317219c4264
Sergey Bylokhov <serb@openjdk.org>
2023-05-25 00:23:35 +0000

    8072678: Wrong exception messages in java.awt.color.ICC_ColorSpace
    Backport-of: f4fa68a2a795ca073409b76332037cc601e66b13

--- jdk/src/share/classes/java/awt/color/ICC_ColorSpace.java
+++ jdk/src/share/classes/java/awt/color/ICC_ColorSpace.java
@@ -558,7 +558,7 @@ public class ICC_ColorSpace extends ColorSpace {
     public float getMinValue(int component) {
         if ((component < 0) || (component > this.getNumComponents() - 1)) {
             throw new IllegalArgumentException(
-                "Component index out of range: + component");
+                "Component index out of range: " + component);
         }
         return minVal[component];
     }
@@ -583,7 +583,7 @@ public class ICC_ColorSpace extends ColorSpace {
     public float getMaxValue(int component) {
         if ((component < 0) || (component > this.getNumComponents() - 1)) {
             throw new IllegalArgumentException(
-                "Component index out of range: + component");
+                "Component index out of range: " + component);
         }
         return maxVal[component];
     }
